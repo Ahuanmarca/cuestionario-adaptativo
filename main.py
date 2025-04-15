5b15b1993cd71dbd99d82af228682a7a4e89c08e
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2025-04-15 10:34:58 +0200

    Usar mini.txt con pocos bloques para pruebas.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from scripts import script010_preparar as script01
 
 def main():
-    with open("temario/temario.txt", "r", encoding="utf-8") as f:
+    with open("temario/mini.txt", "r", encoding="utf-8") as f:
         temario = f.read()
 
     bloques_informacion = script01.main(temario)
 
-    with open("archivo_salida.txt", "w", encoding="utf-8") as f:
+    with open("mini_salida.txt", "w", encoding="utf-8") as f:
 
         f.write(bloques_informacion)
         
 
-    print(f"Archivo normalizado guardado en: archivo_salida.txt")
+    print(f"Archivo normalizado guardado en: mini_salida.txt")
 
 if __name__ == "__main__":
     main()
